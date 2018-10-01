f7ca0f21300ac84c084590554262f3c50933c8fd
Muhammad Ali <raju4uiu@gmail.com>
2018-10-01 20:26:23 +0600

    inital commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blog</title>
+</head>
+<body>
+
+    <h3>Heading 3</h3>
+    <p>Game is on man, i am running laravel 5.6 and bingo!</p>
+    
+</body>
+</html>
